ae715a8a997426f51781f212aa2d4487fae9b59e
s19011 <s19011@std.it-college.ac.jp>
2020-02-25 08:14:00 +0000

    Updates

---
+++ precache-manifest.0cc8d4b0de2f964ba36f728e4f373af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4ff8ed0c9c7dc3045eab6ddafa3ad6",
+    "url": "/test-sanple/index.html"
+  },
+  {
+    "revision": "99d2f8be94bcbd3bf213",
+    "url": "/test-sanple/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5d17ab9175114ae2bef2",
+    "url": "/test-sanple/static/js/2.183bbd5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-sanple/static/js/2.183bbd5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d2f8be94bcbd3bf213",
+    "url": "/test-sanple/static/js/main.7ef5d6e0.chunk.js"
+  },
+  {
+    "revision": "26c9e164a539bf364225",
+    "url": "/test-sanple/static/js/runtime-main.b2b88f96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-sanple/static/media/logo.5d5d9eef.svg"
+  }
+]);
